962caf8c82a285389555e162d011d1f965ea14d1
zeev mindali <mindalizeev@gmail.com>
2022-08-14 15:08:23 +0300

    pdf

---
+++ bsm1408/src/MyJava/MySupplier.java
@@ -0,0 +1,2 @@
+package MyJava;public class MySupplier {
+}
--- bsm1408/src/beans/Person.java
+++ bsm1408/src/beans/Person.java
@@ -1,26 +1,2 @@
-package beans;
-
-public class Person {
-    private final String name;
-    private final  Gender gender;
-
-    public Person(String name, Gender gender) {
-        this.name = name;
-        this.gender = gender;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", gender=" + gender +
-                '}';
-    }
-    public Gender getGender() {
-        return gender;
-    }
+package beans;public class Person {
 }
